20c809ce560b709f833095755a483767bf37b85a
Sejo Basic <sejoob@gmail.com>
2023-01-17 10:59:03 -0700

    Added media query for Responsive styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,8 +5,8 @@ function Form() {
     <main className='form-container'>
       <form>
         <div className='input-wrapper'>
-          <input type='text' placeholder='Top text' />
-          <input type='text' placeholder='Bottom text' />
+          <input type='text' placeholder='Top Text' />
+          <input type='text' placeholder='Bottom Text' />
         </div>
         <button>Get a new meme</button>
       </form>
