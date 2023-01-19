20740a694684733269da1223406e42738b15f0e4
Sejo Basic <sejoob@gmail.com>
2023-01-19 13:56:10 -0700

    Implemented controlled inputs storing values in state and updating text on meme image

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,62 @@
 import React from 'react'
+import { useState } from 'react'
+import memesData from '../utils/memesData'
 
 function Form() {
+  const [meme, setMeme] = useState({
+    topText: '',
+    bottomText: '',
+    randomImg: 'http://i.imgflip.com/1bij.jpg',
+  })
+  const [allMemeImgs, setAllMemeImgs] = useState(memesData)
+
+  function getMemeImage() {
+    const memesArray = allMemeImgs.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+
+    setMeme((prevState) => {
+      return {
+        ...prevState,
+        randomImg: url,
+      }
+    })
+  }
+  function handleChange(event) {
+    setMeme((prevState) => {
+      return {
+        ...prevState,
+        [event.target.name]: event.target.value,
+      }
+    })
+  }
+
   return (
     <main className='form-container'>
-      <form>
+      <div>
         <div className='input-wrapper'>
-          <input type='text' placeholder='Top Text' />
-          <input type='text' placeholder='Bottom Text' />
+          <input
+            type='text'
+            placeholder='Top Text'
+            name='topText'
+            value={meme.topText}
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            placeholder='Bottom Text'
+            name='bottomText'
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
         </div>
-        <button>Get a new meme</button>
-      </form>
+        <button onClick={getMemeImage}>Create a new meme</button>
+      </div>
+      <div className='img-container'>
+        <img src={meme.randomImg} alt='meme image' />
+        <h2 className='top'>{meme.topText}</h2>
+        <h2 className='bottom'>{meme.bottomText}</h2>
+      </div>
     </main>
   )
 }
